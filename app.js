a0245e184104b5e52dee84f5d088b90d135c19c7
joaopedro_ig <110845169+jpedro1711@users.noreply.github.com>
2026-03-27 15:59:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World, using deploy from code.</h1>');
+  res.send('<h2>Hello World, using deploy from code.</h2>');
 });
 
 app.listen(PORT, () => {
